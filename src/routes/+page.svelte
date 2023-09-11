b5c04c1259c9d30b818f551250c0ba8c0661360f
Shauryan  K <shauryan.kanaujia@gmail.com>
2023-09-11 16:38:56 -0700

    placeholder stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-screen h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+		<h1 class="h1">Welcome to Cupertino High School Bike Club!</h1>
+	
+	<p class="font-bold container h-full mx-auto flex justify-center items-center">Stay in the loop with our socials:</p>
+	<a href="https://www.instagram.com/cupertinobikeclub">
+	<ul class="container h-full mx-auto flex justify-center items-center">
+		<li><code class="code underline">@cupertinobikeclub</code> on instagram.</li>
+	</ul>
+	</a>
+	<a href="discord.gg/BS8ubqNjVY">
+	<ul class="container h-full mx-auto flex justify-center items-center">
+		<li><code class="code underline">discord.gg/BS8ubqNjVY</code> is our discord server.</li>
+	</ul>
+	</a>
+</div>
 </div>
